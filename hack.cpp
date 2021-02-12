a8a89d049f46dd73e49eb5c27648324e3f3a0a2a
OzaronHack <78981138+OzaronHack@users.noreply.github.com>
2021-02-12 22:34:15 +0300

    hackbyozaRNO
    Judgrive hacked

---
+++ hack.cpp
@@ -0,0 +1,32 @@
+// HACKED BY OZARON BSENDPACKET GUGDRIVE HACK BY OZARON MY GITHUB BSENDPACKET VISIT IT PLZ GUYS SUBSCUTIR ME AND LIKE FORK ME YEEEEA FUCJ ME
+
+#include OZARNOHACK bSenDPacket()->SendPacke
+#define <ozaron.dll>
+
+using name usingspace hackozaron;
+
+// ENTRY POINT HACKED BY OZARON OBFUSCATION BY OZARTON
+
+class OzaronHackBy{
+private:
+    Fitter(){""  hackOzarno*** = new OzaronHackBy("a" hackedBy) // Sup guys, my name is Ozaron and im a LUA coder. Sometimes im drop my script here. }
+
+    Detonator( __fastcall hackOzaron )(){ bSenDPacket->SendPacket(->BsendAcket**)[__cdecl *** sendPacket . BsenDPacket(())[]] }
+}
+
+// HACK BY SKEET GAMESENSESES>PUBY?OZARON HACK BY HACK
+
+main __OZARON( __stdcall WINAPI ozaronBaked, __cdecl )
+{
+    return OZARON;
+
+    hackbSendPacket->FindSignature( main, "____OZARIN", "BAYANAT", "ntdll.dll" )->hackSendPacket( ****bSendPacket &&&&&BATANAT )
+
+    OzaronHackBy->HackOzaronEntryPoint()->bSendPacket;
+
+    // Sup guys, my name is Ozaron and im a LUA coder. Sometimes im drop my script here.
+
+    bSendPacket-()>SendPacket-(>BsendPacket.SendPacket)[exepacket](*******),,,;
+}
+
+// HACK BY OZARON WRITED AUTHORIZED RIGHTS MY SCRIPT
